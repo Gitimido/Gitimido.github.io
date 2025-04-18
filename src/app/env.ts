fb5e4ba904709797e44a7b17d85939c498a04daf
Your Name <you@example.com>
2025-04-18 22:03:51 +0200

    update path

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,4 +1,4 @@
 export const environment = {
   workersApi: 'https://my-worker-project.mohamedhazem789.workers.dev/',
-  profile: 'https://gitimido.github.io/profile/',
+  profile: 'https://mohamedshazem.github.io/profile/',
 };
